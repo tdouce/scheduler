7c2aeb21e6cc8db0fc96aa5fad7421050116ee58
Travis Douce <travisdouce@gmail.com>
2011-11-28 16:08:22 -0500

    Created errormessage partial that roles and employees share

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,9 @@
 class Role < ActiveRecord::Base
+
+  attr_accessible :name
+  
+  validates :name, :presence => true 
+
   #has_many  :employments
   #has_many  :employees, :through => :employments
 end
