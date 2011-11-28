7edcde9816298936ff7d2a2fd51cb6a927cbf313
Travis Douce <travisdouce@gmail.com>
2011-11-28 11:45:46 -0500

    Adds update action to employee.  Puts form in partial for 'edit' and 'update' views.  Adds error explanations on 'new' view

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,34 +4,26 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
   end
 
-  # The html form for creating a new employee
-  def new 
-
-    # ':employee' get the nested dictionary (i.e. all the form values)
-    @employee = Employee.new(params[:employee])
-
-  end
-
-  def update
+  # Individual employees
+  def show
     @employee = Employee.find(params[:id])
   end
 
-  def destroy
-    Employee.find(params[:id]).destroy
-    flash[:success] = "Employee was destroyed."
-
-    # Redirect to index page
-    redirect_to employees_path
-
+  # The html form for creating a new employee
+  def new 
+    # ':employee' get the nested dictionary (i.e. all the form values)
+    @employee = Employee.new(params[:employee])
   end
 
   # The POST http verb for the actual creation of a new employee
   def create 
+    # Saving the ':employee' and all the form values, which is in a nested
+    # dictionary
     @employee = Employee.new(params[:employee])
 
     if @employee.save
+      flash[:success] = "Employee #{ @employee.full_name} was created!"
 
-      flash[:success] = "Employee created!"
       # Redirect to employee index
       redirect_to employees_url
     else
@@ -41,22 +33,34 @@ class EmployeesController < ApplicationController
       # Why not something like this instead of " render 'new' "?
       #render new_employee_url 
     end
-
   end
 
-  # Individual employees
-  def show
+  # Used in the PUT http request to actually update the employee in the datbase
+  def update
+    # Find the same employee that was used in the 'edit' action. 
+    @employee = Employee.find(params[:id])
+
+    # If the saving of the employee is successfull
+    if @employee.update_attributes(params[:employee])
+      flash[:success] = "Employee #{@employee.full_name} was updated"
+      redirect_to employees_url
+    else
+      flash[:failure] = "Employee was NOT updated"
+      render 'edit'
+    end
+  end
+
+  # Used in the html form. Find the employee and it's attributes
+  def edit
     @employee = Employee.find(params[:id])
   end
 
-  def edit
+  def destroy
+    employee = Employee.find(params[:id]).destroy
+    flash[:success] = "Employee #{employee.full_name} was destroyed."
+
+    # Redirect to index page
+    redirect_to employees_path
   end
-
-  def update
-  end
-
-
-
-
 
 end
