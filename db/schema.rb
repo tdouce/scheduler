0d86c259623f85c1e2ffe01df1aa45d80bd5213f
Travis Douce <travisdouce@gmail.com>
2011-12-22 09:17:19 -0500

    Runs migration to create shifts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111211181349) do
+ActiveRecord::Schema.define(:version => 20111222141059) do
 
   create_table "employees", :force => true do |t|
     t.string   "first_name"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(:version => 20111211181349) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "shifts", :force => true do |t|
+    t.integer  "employee_id"
+    t.integer  "role_id"
+    t.datetime "dt_start"
+    t.datetime "dt_end_"
+    t.string   "template_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
