0d976dbe373980feaf980d11409cb4762da59544
Travis Douce <travisdouce@gmail.com>
2011-12-22 19:37:36 -0500

    Adds attr_accesible attributes to model

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,7 @@
 class Shift < ActiveRecord::Base
+
+  attr_accessible :name, :time_start, :time_end
+
   validates :name, :presence => true,
             :uniqueness => true
   validates :time_start, :presence => true
